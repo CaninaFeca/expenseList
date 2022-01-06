28a57f9e6e6b1673fea62e956622f90fc63d6758
Can İnal <inalcan@gmail.com>
2022-01-06 19:44:55 +0300

    3-40

---
+++ src/components/ExpenseSheet.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpenseSheet.css";
+import Card from "./Card";
+
+function ExpenseSheet(props) {
+  return (
+    <Card className="expenses">
+      {props.items.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
+    </Card>
+  );
+}
+
+export default ExpenseSheet;
